49fe77c8b18a7564c3f5b9b6edb85453083c7123
Ayham Mamoun <ayhamboualfadl@gmail.com>
2018-09-02 19:30:24 +0300

    Code

--- Drone_Remote_Code/Drone-Remote-Lib.h
+++ Drone_Remote_Code/Drone-Remote-Lib.h
@@ -7,8 +7,20 @@
 #define LEFT_JS_Y A1
 #define RIGHT_JS_X A2
 #define RIGHT_JS_Y A3
+#define RIGHT_JS_SW 13
+#define LEFT_JS_SW 12
 
 #define POWER_3.3V 2
+
+
+#define CONNECTION_STR "<START>"
+#define END_CONNECTION_STR "<END>"
+
+#define HOVER_STR "<HOVER>"
+#define AUTO_LAND "<LAND>"
+
+#define PING_DRONE_STR "<PING?>"
+#define ALTITUDE_DRONE_STR "<ALTITUDE?>"
 
 class Remote 
 {
@@ -18,9 +30,22 @@ class Remote
         void CheckConnection();
         void PingDrone();
 
+        void IgniteEngines();
+        void IDLE_Engine();
+        void SetClimbRate(float rate = 0.3);
+        void ShutDownEngines();        
+
         void RequestAutoLand();
+        void StartClimb();
         void HoverPlane();
-
+        
+        float GetAltitude();
+        float GetDistance();
     private:
-
+        bool connected = false;
+        bool inAir = false;
+        bool hoverMode = false;
+        bool enginesIgnited = false;
+        bool engine_idled = false;
+        float approxDist = 0.0f;
 }
